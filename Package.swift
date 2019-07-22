3e7aead4542a723abe40bd7c32e88858baa93b78
David W. Rankin Jr <david@bohemians.us>
2019-07-21 23:12:11 -0400

    Swift 4.0 Package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "SwiftAbstractLogger"
+    name: "SwiftAbstractLogger",
+    products: [
+        .library(name: "SwiftAbstractLogger", targets: ["SwiftAbstractLogger"])
+    ],
+    targets: [
+        .target(name: "SwiftAbstractLogger", path: "SwiftAbstractLogger" )
+    ]
 )
